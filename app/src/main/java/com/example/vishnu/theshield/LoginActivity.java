82f1c0c5530279fbb957d57d0052040584ca4684
vishnutdeepak <vishnu.tdeepak@gmail.com>
2018-04-18 00:53:48 +0530

    Splash Activity complete

--- app/src/main/java/com/example/vishnu/theshield/LoginActivity.java
+++ app/src/main/java/com/example/vishnu/theshield/LoginActivity.java
@@ -352,5 +352,6 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             showProgress(false);
         }
     }
+
 }
 
--- app/src/main/java/com/example/vishnu/theshield/Splash.java
+++ app/src/main/java/com/example/vishnu/theshield/Splash.java
@@ -1,13 +1,45 @@
 package com.example.vishnu.theshield;
 
+import android.content.Context;
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+import java.util.concurrent.TimeUnit;
 
 public class Splash extends AppCompatActivity {
-
+    private Handler mHandler = new Handler();
+Context ct;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        ActionBar ab = getSupportActionBar();
+        ab.hide();
         setContentView(R.layout.activity_splash2);
+        mHandler.postDelayed(mUpdateTimeTask, 1500);
+ct = this.getApplicationContext();
+
+
+
+
     }
+
+    private Runnable mUpdateTimeTask = new Runnable() {
+        public void run() {
+            // do what you need to do here after the delay
+            Intent intent = new Intent(ct, LoginActivity.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP| Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+
+            startActivity(intent);
+        }
+
+    };
+
 }
